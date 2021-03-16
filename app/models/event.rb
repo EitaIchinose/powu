bb84ab91f4354c1fa6380c2513f5abc584bba7f8
EitaIchinose <dellps.psdell@gmail.com>
2021-03-16 14:14:07 +0900

    eventモデル　バリデーション設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :department_events
   has_many :departments, through: :department_events
+
+  validates :title, presence: true
+  validates :start_time, presence: true
+  validates :content, presence: true
+  validates :user, presence: true
 end
