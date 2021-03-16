237bd80915a1804aba4bfdc5e90bafd032325291
EitaIchinose <dellps.psdell@gmail.com>
2021-03-16 15:13:13 +0900

    予定表削除機能・実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "departments#index"
   resources :users,  only: [:edit, :update]
-  resources :departments, only: [:new, :create] do
+  resources :departments, only: [:new, :create, :destroy] do
     resources :events, only: [:index, :new, :create]
   end
 end
