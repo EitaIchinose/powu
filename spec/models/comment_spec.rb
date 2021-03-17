6de2c880e1a2102717c331768d924e14271a7667
EitaIchinose <dellps.psdell@gmail.com>
2021-03-17 11:26:28 +0900

    モデル単体テスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,16 +7,39 @@ RSpec.describe Comment, type: :model do
 
   describe 'コメント送信' do
     context 'コメントを送信出来る時' do
-      it 'textがあればコメントを送信できる' do
+      it 'text、画像が存在すればコメントを送信できる' do
+        expect(@comment).to be_valid
+      end
+
+      it 'textが空でもコメントを送信できる' do
+        @comment.text = ''
+        expect(@comment).to be_valid
+      end
+
+      it '画像が空でもコメントを送信できる' do
+        @comment.image = nil
         expect(@comment).to be_valid
       end
     end
 
     context 'コメントを送信出来ない時' do
-      it 'textが空の場合、送信できない' do
+      it 'textと画像が空の場合、送信できない' do
         @comment.text = ''
+        @comment.image = nil
         @comment.valid?
         expect(@comment.errors.full_messages).to include('文を入力してください')
+      end
+
+      it 'departmentが紐付いていないと、送信できない' do
+        @comment.department = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Departmentを入力してください")
+      end
+
+      it 'userが紐付いていないと、送信できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Userを入力してください")
       end
     end
   end
