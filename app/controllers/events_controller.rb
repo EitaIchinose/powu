bbbdba3646bbcb1567ed954232af7cf234614b4d
EitaIchinose <dellps.psdell@gmail.com>
2021-03-18 16:26:06 +0900

    予定内容編集機能の実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,13 +23,18 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:department_id])
+    @event = Event.find(params[:id])
+    @department = Department.find(params[:department_id])
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to department_events_path
   end
 
   def destroy
@@ -38,11 +43,12 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :content, :start_time).merge(user_id: current_user.id,
+    params.require(:event).permit(:title, :content, :start_time, :id).merge(user_id: current_user.id,
                                                                        department_id: params[:department_id])
   end
 
   def datetime_params
-    params.require(:event).permit(:start_time)
+    params.permit(:title, :content, :start_time).merge(user_id: current_user.id,
+      department_id: params[:department_id])
   end
 end
