e293221b59339e74d1802019e47dd2280e9d06b0
EitaIchinose <dellps.psdell@gmail.com>
2021-03-19 22:26:45 +0900

    吹き出し機能（未実装の為、コメントアウト）

---
+++ app/javascript/test.js
@@ -0,0 +1,25 @@
+// const moment = require("moment");
+
+// $(window).on('load', function() {
+//     $.each(gon.event, function(index,value) {
+//       var time = new Date(value.start_time)
+//       var nowTime = moment(time).format()
+//       var tooltipHtml = `
+//       <div>
+//         <div class="tooltipClose">×</div>
+//           <div style="margin: 10px;">
+//             <div>
+//             予定：${value.title}<br>
+//               ${nowTime}<br>
+//             </div>
+//           </div>
+//         </div>
+//       </div>`;
+
+//         $(".showTooltip").tooltip({
+//             content: tooltipHtml
+//         }) 
+//     });
+// });
+
+//上記・未実装（カレンダー吹き出し機能）
