4caaf5d75b8c49e8482238689f73ea734de9ef66
EitaIchinose <dellps.psdell@gmail.com>
2021-03-22 18:45:00 +0900

    予定表（週）実装

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -23,3 +23,17 @@
 // });
 
 //上記・未実装（カレンダー吹き出し機能）
+
+// タブ機能
+$(function() {
+  $('.tab-btn').on('click', function() {
+    var tabWrap = $(this).parents('.tab-wrap');
+    var tabBtn = tabWrap.find(".tab-btn");
+    var tabContents = tabWrap.find('.tab-contents');
+    tabBtn.removeClass('show');
+    $(this).addClass('show');
+    var elmIndex = tabBtn.index(this);
+    tabContents.removeClass('show');
+    tabContents.eq(elmIndex).addClass('show');
+  });
+});
